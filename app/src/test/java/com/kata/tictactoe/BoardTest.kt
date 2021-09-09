f1f8d8f257fdf805315742a4605630ec0cebd1e8
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 20:57:31 +0530

    <Test Case> player2 wins when left diagonal combination is found in the board

--- app/src/test/java/com/kata/tictactoe/BoardTest.kt
+++ app/src/test/java/com/kata/tictactoe/BoardTest.kt
@@ -345,6 +345,27 @@ class BoardTest {
         assertThat(expectedResult).isEqualTo(winner)
     }
 
+    /* combination
+    O X 0
+    X O 0
+    X 0 O
+    */
+    @Test
+    fun test_if_player2_can_win_if_left_diagonal_combination_is_found() {
+        val expectedResult = true
+
+        board.updateCellsValue(1)
+        board.updateCellsValue(0)
+        board.updateCellsValue(3)
+        board.updateCellsValue(4)
+        board.updateCellsValue(6)
+        board.updateCellsValue(8)
+
+        val winner = board.hasWinner()
+
+        assertThat(expectedResult).isEqualTo(winner)
+    }
+
     private fun getInitialBoardState(): List<String> {
         val gameBoard = mutableListOf<String>()
         for (r in 0..8) {
