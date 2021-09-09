cc9dc42e478bfc40453fca8cef69132bfd17031f
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 21:06:20 +0530

    <Code Refactor> moved combinations logic into single winning combination logic and removed duplicate code

--- app/src/main/java/com/kata/tictactoe/Board.kt
+++ app/src/main/java/com/kata/tictactoe/Board.kt
@@ -34,60 +34,24 @@ class Board {
     }
 
     fun hasWinner(): Boolean {
-        var hasWinner = false
-
-        val horizontalCombination = getHorizontalCombination()
-        val verticalCombination = getVerticalCombination()
-        val diagonalCombination = getDiagonalCombination()
-
-        when {
-            horizontalCombination != null && horizontalCombination.isNotEmpty() -> {
-                hasWinner = true
-            }
-            verticalCombination != null && verticalCombination.isNotEmpty() -> {
-                hasWinner = true
-            }
-            diagonalCombination != null && diagonalCombination.isNotEmpty() -> {
-                hasWinner = true
-            }
-        }
-        return hasWinner
+        val winningCombinations = getWinningCombination()
+        return (winningCombinations != null && winningCombinations.isNotEmpty())
     }
 
     /*
-   * checks if any horizontal combinations is found from given cell combinations
+   * checks if any combinations is found from given cell combinations
    * firstOrNull returns the array combination if a match is found
    * */
-    private fun getHorizontalCombination(): IntArray? {
-        val horizontalCombinations =
-            listOf(intArrayOf(0, 1, 2), intArrayOf(3, 4, 5), intArrayOf(6, 7, 8))
-        return horizontalCombinations.firstOrNull {
-            cellsValue[it[0]] == cellsValue[it[1]] && cellsValue[it[0]] == cellsValue[it[2]]
-                    && cellsValue[it[0]] != "0"
-        }
-    }
-
-    /*
-    * checks if any vertical combinations is found from given cell combinations
-    */
-    private fun getVerticalCombination(): IntArray? {
-        val verticalCombination =
-            listOf(intArrayOf(0, 3, 6), intArrayOf(1, 4, 7), intArrayOf(2, 5, 8))
-
-        return verticalCombination.firstOrNull {
-            cellsValue[it[0]] == cellsValue[it[1]] && cellsValue[it[0]] == cellsValue[it[2]]
-                    && cellsValue[it[0]] != "0"
-        }
-    }
-
-    /*
-   * checks if any vertical combinations is found from given cell combinations
-   */
-    private fun getDiagonalCombination(): IntArray? {
-        val diagonalCombination =
-            listOf(intArrayOf(0, 4, 8), intArrayOf(2, 4, 6))
-
-        return diagonalCombination.firstOrNull {
+    private fun getWinningCombination(): IntArray? {
+        val winningCombinations = listOf(
+            //horizontal combinations
+            intArrayOf(0, 1, 2), intArrayOf(3, 4, 5), intArrayOf(6, 7, 8),
+            //Vertical combinations
+            intArrayOf(0, 3, 6), intArrayOf(1, 4, 7), intArrayOf(2, 5, 8),
+            //diagonal combinations
+            intArrayOf(0, 4, 8), intArrayOf(2, 4, 6)
+        )
+        return winningCombinations.firstOrNull {
             cellsValue[it[0]] == cellsValue[it[1]] && cellsValue[it[0]] == cellsValue[it[2]]
                     && cellsValue[it[0]] != "0"
         }
