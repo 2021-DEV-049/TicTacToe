c468cf3da968b815eb07fc41dc04ee2c544b48ce
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 22:03:07 +0530

    designed entry fragment layout and removed setcontentview from MainActivity

--- app/src/main/java/com/kata/tictactoe/ui/fragments/EntryFragment.kt
+++ app/src/main/java/com/kata/tictactoe/ui/fragments/EntryFragment.kt
@@ -5,15 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.kata.tictactoe.R
+import com.kata.tictactoe.databinding.FragmentEntryBinding
 
 class EntryFragment : Fragment() {
+
+    private var _binding: FragmentEntryBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_entry, container, false)
+    ): View {
+        _binding = FragmentEntryBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.playBtn.setOnClickListener {
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
