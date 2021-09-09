25d44ccbe09f81b0dd17a74f7f5245db24373b06
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 22:30:51 +0530

    created game view model and configured in koin

--- app/src/main/java/com/kata/tictactoe/GameApplication.kt
+++ app/src/main/java/com/kata/tictactoe/GameApplication.kt
@@ -2,6 +2,7 @@ package com.kata.tictactoe
 
 import android.app.Application
 import com.kata.tictactoe.ui.viewmodel.EntryViewModel
+import com.kata.tictactoe.ui.viewmodel.GameViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -12,11 +13,16 @@ class GameApplication : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@GameApplication)
-            modules(listOf(viewModelModule))
+            modules(listOf(appModule, viewModelModule))
         }
     }
 }
 
+val appModule = module {
+    factory { Board() }
+}
+
 val viewModelModule = module {
     viewModel { EntryViewModel() }
+    viewModel { GameViewModel(get()) }
 }
---
+++ app/src/main/java/com/kata/tictactoe/ui/viewmodel/GameViewModel.kt
@@ -0,0 +1,7 @@
+package com.kata.tictactoe.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.kata.tictactoe.Board
+
+class GameViewModel(private val board: Board) : ViewModel() {
+}
