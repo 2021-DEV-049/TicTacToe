68c2c1743e3c83b745c53eb0acd169f5305a7a6c
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 22:14:12 +0530

    created test case to check if live data returns false when one of the fields is empty

--- app/src/main/java/com/kata/tictactoe/ui/viewmodel/EntryViewModel.kt
+++ app/src/main/java/com/kata/tictactoe/ui/viewmodel/EntryViewModel.kt
@@ -1,6 +1,22 @@
 package com.kata.tictactoe.ui.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class EntryViewModel: ViewModel() {
+class EntryViewModel : ViewModel() {
+
+    private var _areFieldsValid = MutableLiveData<Boolean>()
+    val areFieldsValid: LiveData<Boolean> = _areFieldsValid
+
+    fun checkIfFieldsAreValid(player1Name: String, player2Name: String) {
+
+        if ((player1Name.trim { it <= ' ' }).isBlank() ||
+            (player2Name.trim { it <= ' ' }).isBlank()
+        ) {
+            _areFieldsValid.postValue(false)
+        } else {
+            _areFieldsValid.postValue(true)
+        }
+    }
 }
--- app/src/test/java/com/kata/tictactoe/ui/viewmodel/EntryViewModelTest.kt
+++ app/src/test/java/com/kata/tictactoe/ui/viewmodel/EntryViewModelTest.kt
@@ -1,7 +1,10 @@
 package com.kata.tictactoe.ui.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import com.kata.tictactoe.getOrAwaitValue
 import org.junit.Rule
+import org.junit.Test
 
 class EntryViewModelTest {
 
@@ -9,4 +12,14 @@ class EntryViewModelTest {
     val rule = InstantTaskExecutorRule()
 
     private val viewModel = EntryViewModel()
+
+    @Test
+    fun test_should_return_false_if_one_of_the_field_is_empty() {
+
+        viewModel.checkIfFieldsAreValid("Player1", "")
+
+        val result = viewModel.areFieldsValid.getOrAwaitValue()
+
+        assertThat(result).isFalse()
+    }
 }
