d8398397f6a8535fc3b6acbd5018f5eaa6f530b8
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 22:06:07 +0530

    configured koin in application class

--- app/src/main/java/com/kata/tictactoe/GameApplication.kt
+++ app/src/main/java/com/kata/tictactoe/GameApplication.kt
@@ -1,6 +1,15 @@
 package com.kata.tictactoe
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class GameApplication: Application() {
+class GameApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidContext(this@GameApplication)
+            modules()
+        }
+    }
 }
