e230ce6eb164ee0ee038b8c2e852927441c4caa8
2021-DEV-049 <2021dev049@gmail.com>
2021-09-10 01:35:25 +0530

    added alert dialog as an extension function

--- app/src/main/java/com/kata/tictactoe/utils/Utils.kt
+++ app/src/main/java/com/kata/tictactoe/utils/Utils.kt
@@ -1,6 +1,9 @@
 package com.kata.tictactoe.utils
 
+import android.content.Context
 import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.kata.tictactoe.R
@@ -26,4 +29,27 @@ fun View.showSnackBar(message: String, errorMessage: Boolean) {
         )
     }
     snackBar.show()
+}
+
+fun Context.alertDialog(
+    positiveText: String = "Ok",
+    negativeText: String = "",
+    message: String = "",
+    callback: () -> Unit
+) {
+    val dialog = AlertDialog.Builder(this)
+        .setCancelable(false)
+        .setMessage(message)
+        .setPositiveButton(positiveText) { _, _ ->
+            callback()
+        }
+        .setNegativeButton(negativeText) { _, _ ->
+        }
+        .create()
+
+    dialog.show()
+
+    val textView = dialog.findViewById<View>(android.R.id.message) as TextView
+    textView.textSize = 20f
+
 }
