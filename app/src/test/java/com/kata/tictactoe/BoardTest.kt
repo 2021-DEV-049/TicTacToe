4999eadf4d1756e3a0c7c753d15f9d8412d3f712
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 20:30:09 +0530

    <Test Case> player2 wins when second horizontal combination is found in the board

--- app/src/test/java/com/kata/tictactoe/BoardTest.kt
+++ app/src/test/java/com/kata/tictactoe/BoardTest.kt
@@ -138,6 +138,29 @@ class BoardTest {
         assertThat(expectedResult).isEqualTo(winner)
     }
 
+    /* combination
+       X X 0
+       O O O
+       X X 0
+      */
+    @Test
+    fun test_if_player2_can_win_if_second_horizontal_combination_is_found() {
+        val expectedResult = true
+
+        board.updateCellsValue(0)
+        board.updateCellsValue(3)
+        board.updateCellsValue(1)
+        board.updateCellsValue(2)
+        board.updateCellsValue(6)
+        board.updateCellsValue(4)
+        board.updateCellsValue(7)
+        board.updateCellsValue(5)
+
+        val winner = board.hasWinner()
+
+        assertThat(expectedResult).isEqualTo(winner)
+    }
+
     private fun getInitialBoardState(): List<String> {
         val gameBoard = mutableListOf<String>()
         for (r in 0..8) {
