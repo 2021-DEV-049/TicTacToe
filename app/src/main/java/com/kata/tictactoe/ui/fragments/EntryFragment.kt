cbb2d34e0d37790b71364db019e2145119ecfae0
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 22:24:25 +0530

    added field validation and handled navigation, error scenarios.

--- app/src/main/java/com/kata/tictactoe/ui/fragments/EntryFragment.kt
+++ app/src/main/java/com/kata/tictactoe/ui/fragments/EntryFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.kata.tictactoe.R
 import com.kata.tictactoe.databinding.FragmentEntryBinding
 import com.kata.tictactoe.ui.viewmodel.EntryViewModel
+import com.kata.tictactoe.utils.showSnackBar
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class EntryFragment : Fragment() {
@@ -29,12 +32,32 @@ class EntryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.areFieldsValid.observe(viewLifecycleOwner) {
-
+        viewModel.areFieldsValid.observe(viewLifecycleOwner) { isValid ->
+            when (isValid) {
+                true -> navigate()
+                false -> showErrorMessage()
+            }
         }
 
         binding.playBtn.setOnClickListener {
+            player1Name = binding.etPlayer1.text.toString()
+            player2Name = binding.etPlayer2.text.toString()
+            viewModel.checkIfFieldsAreValid(player1Name, player2Name)
         }
+    }
+
+    private fun navigate() {
+        val actions = EntryFragmentDirections.actionEntryFragmentToGameFragment(
+            player1Name, player2Name
+        )
+        findNavController().navigate(actions)
+    }
+
+    private fun showErrorMessage() {
+        binding.root.showSnackBar(
+            getString(R.string.enter_all_fields),
+            true
+        )
     }
 
     override fun onDestroy() {
