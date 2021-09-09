a4a44a7c7e57ed530ee35e8726a6191bb74571c8
2021-DEV-049 <2021dev049@gmail.com>
2021-09-09 21:49:07 +0530

    added two empty fragments Entry and Game fragment

---
+++ app/src/main/java/com/kata/tictactoe/ui/fragments/EntryFragment.kt
@@ -0,0 +1,19 @@
+package com.kata.tictactoe.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kata.tictactoe.R
+
+class EntryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_entry, container, false)
+    }
+}
