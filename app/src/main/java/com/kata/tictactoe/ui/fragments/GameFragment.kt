29035752121f7b33fe3fc652abef86ca1a125915
2021-DEV-049 <2021dev049@gmail.com>
2021-09-10 01:22:24 +0530

    assigned name for players received from fragment bundle

--- app/src/main/java/com/kata/tictactoe/ui/fragments/GameFragment.kt
+++ app/src/main/java/com/kata/tictactoe/ui/fragments/GameFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.kata.tictactoe.R
 import com.kata.tictactoe.databinding.FragmentGameBinding
 
@@ -13,12 +14,26 @@ class GameFragment : Fragment() {
     private var _binding: FragmentGameBinding? = null
     private val binding get() = _binding!!
 
+    private val args: GameFragmentArgs by navArgs()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentGameBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initializePlayerNames()
+    }
+
+    private fun initializePlayerNames() {
+        val player1 = args.player1Name
+        val player2 = args.player2Name
+
+        binding.player1Name.text = player1
+        binding.player2Name.text = player2
     }
 
     override fun onDestroy() {
