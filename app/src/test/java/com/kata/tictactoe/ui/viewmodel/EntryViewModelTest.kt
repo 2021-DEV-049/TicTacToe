9a3d856153f9a5e7ac55f586fb7bad1ac69dabd3
2021-DEV-049 <2021dev049@gmail.com>
2021-09-10 11:10:11 +0530

    <Code Refactor> replaced strings with constants in entry viewmodel

--- app/src/test/java/com/kata/tictactoe/ui/viewmodel/EntryViewModelTest.kt
+++ app/src/test/java/com/kata/tictactoe/ui/viewmodel/EntryViewModelTest.kt
@@ -16,7 +16,7 @@ class EntryViewModelTest {
     @Test
     fun test_should_return_false_if_one_of_the_field_is_empty() {
 
-        viewModel.checkIfFieldsAreValid("Player1", "")
+        viewModel.checkIfFieldsAreValid(Player1, "")
 
         val result = viewModel.areFieldsValid.getOrAwaitValue()
 
@@ -25,10 +25,15 @@ class EntryViewModelTest {
 
     @Test
     fun test_should_return_true_if_all_the_fields_are_filled() {
-        viewModel.checkIfFieldsAreValid("Player1", "Player2")
+        viewModel.checkIfFieldsAreValid(Player1, Player2)
 
         val result = viewModel.areFieldsValid.getOrAwaitValue()
 
         assertThat(result).isTrue()
+    }
+
+    companion object {
+        const val Player1 = "Player1"
+        const val Player2 = "Player2"
     }
 }
